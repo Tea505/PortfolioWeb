a5d504686ee7a38e81597593a6559e1e8d6afd9b
Tea <mtauha9@gmail.com>
2025-07-18 21:07:33 -0400

    working on this again

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
+ document.querySelectorAll('.button-container button').forEach(button => {
+            button.addEventListener('click', () => {
+                console.log(`Clicked button with id: ${button.id}`);
+                const targetId = button.id;
+                const targetSection = document.getElementById(targetId);
+                    if (targetSection) {
+                        targetSection.scrollIntoView({ behavior: 'smooth' });
+                    }
+                });
+            });
+            
 function openModal(id) {
     document.getElementById(id + '-modal').style.display = 'flex';
 }
